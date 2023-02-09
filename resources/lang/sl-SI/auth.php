4fc3d950d65debbf1132d86bd2091877e496127b
leon997 <leonpoberznikynwa@gmail.com>
2023-02-09 15:50:53 +0100

    translate auth, accounts

--- resources/lang/sl-SI/auth.php
+++ resources/lang/sl-SI/auth.php
@@ -17,13 +17,15 @@ return [
     'reset'                 => 'Ponastavi',
     'never'                 => 'nikoli',
     'landing_page'          => 'Ciljna stran',
-    'personal_information'  => 'Zasebni podatki',
+    'personal_information'  => 'Personal Information',
     'register_user'         => 'Registracija uporabnika',
     'register'              => 'Registracija',
+    'sign_up'               => 'Postanite uporabnik',
+    'sign_up_to'            => 'Ustvarite Akaunting račun',
 
     'form_description' => [
-        'personal'          => 'Povezava s povabilom bo poslana novemu uporabniku, zato se prepričajte, da je e-poštni naslov pravilen. Lahko bodo vnesli svoje geslo.',
-        'assign'            => 'Uporabnik bo imel dostop do izbranih podjetij. Dovoljenja lahko omejite na strani <a href=":url" class="border-b border-black">roles</a> .',
+        'personal'          => 'Povezava s povabilom bo poslana novemu uporabniku, zato se prepričajte, da je e-poštni naslov pravilen. Prejemnik bo lahko sam vnesel svoje poljubno geslo.',
+        'assign'            => 'Uporabnik bo imel dostop do izbranih podjetij. Dovoljenja lahko omejite iz <<a href=":url" class="border-b border-black">roles</a> page.',
         'preferences'       => 'Izberite privzeti jezik uporabnika. Ciljno stran lahko nastavite tudi po prijavi uporabnika.',
     ],
 
@@ -39,12 +41,11 @@ return [
     'error' => [
         'self_delete'       => 'Napaka: Ni morete izbrisati samega sebe!',
         'self_disable'      => 'Napaka: Ni morete onemogočiti samega sebe!',
-        'unassigned'        => '
-Napaka: ni mogoče preklicati podjetja! Podjetju :company mora biti dodeljen vsaj en uporabnik.',
+        'unassigned'        => 'Napaka: ni mogoče! Podjetju :company mora biti dodeljen vsaj en uporabnik.        ',
         'no_company'        => 'Napaka: Vašemu računu ni bilo dodeljeno nobeno podjetje. Prosimo, obrnite na skrbnika sistema.',
     ],
 
-    'login_redirect'        => 'Preverjanje opravljeno! Preusmerjamo...',
+    'login_redirect'        => 'Preverjanje opravljeno! Preusmerjeni ste ...',
     'failed'                => 'Podatki ne ustrezajo.',
     'throttle'              => 'Preveč poskusov prijave. Poskusite znova čez: sekund sekund.',
     'disabled'              => 'Ta račun je bil onemogočen. Kontaktirajte administratorja.',
@@ -86,6 +87,42 @@ Prilagodite svoj Akaunting',
         ],
         'accountant' => [
             'name'          => 'Računovodstvo',
+            'description'   => 'Dostopajo lahko do računov, transakcij, poročil in ustvarjajo temeljnice.',
+        ],
+        'employee' => [
+            'name'          => 'Zaposleni',
+            'description'   => 'Ustvarijo lahko zahtevke za stroške in spremljajo čas za dodeljene projekte, vendar lahko vidijo samo svoje podatke.',
+        ],
+    ],
+
+    'invitation' => [
+        'message_1'         => 'To e-pošto ste prejeli, ker ste vabljeni, da se pridružite Akauntingu.',
+        'message_2'         => 'Če se ne želite pridružiti, vam ni treba storiti ničesar več.',
+        'button'            => 'Začnite',
+    ],
+
+    'information' => [
+        'invoice'           => 'Preprosta kreacija računov',
+        'reports'           => 'Ustvarjanje in izvoz podrobnih poročil',
+        'expense'           => 'Beležite vse stroške',
+        'customize'         => 'Preuredite svoj Akaunting po meri',
+    ],
+
+    'roles' => [
+        'admin' => [
+            'name'          => 'Admin',
+            'description'   => 'Dobijo popoln dostop do Akauntinga, vključno s strankami, računi, poročili, nastavitvami in aplikacijami.',
+        ],
+        'manager' => [
+            'name'          => 'Manager',
+            'description'   => 'Dobijo popoln dostop do Akauntinga, vendar ne morejo upravljati uporabnikov in aplikacij.',
+        ],
+        'customer' => [
+            'name'          => 'Stranka',
+            'description'   => 'Lahko dostopajo do portala za stranke in plačujejo svoje račune prek spleta s plačilnimi metodami, ki jih nastavite.',
+        ],
+        'accountant' => [
+            'name'          => 'Računovodja',
             'description'   => 'Dostopajo lahko do računov, transakcij, poročil in ustvarjajo temeljnice.',
         ],
         'employee' => [
