d9c69472aa6cb7efcddbe873dd6867ebbcc244a4
leon997 <leonpoberznikynwa@gmail.com>
2023-04-28 03:26:11 +0200

    rm footer txt

--- resources/views/components/layouts/preview/footer.blade.php
+++ resources/views/components/layouts/preview/footer.blade.php
@@ -2,12 +2,7 @@
 
     <footer class="footer container">
         <div class="flex flex-col sm:flex-row items-center justify-between lg:mt-20 py-7 text-sm font-light">
-            <div>
-                {{ trans('footer.powered') }}:
-                <x-link href="{{ trans('footer.link') }}" target="_blank" override="class">{{ trans('footer.software') }}</x-link>
-                &nbsp;<span class="material-icons align-middle text-black-300">code</span>&nbsp;
-                {{ trans('footer.version') }} {{ version('short') }}
-            </div>
+
         </div>
     </footer>
 
