2d1fcc4828140113a17ab3d2897394f51e87e945
leon997 <leonpoberznikynwa@gmail.com>
2023-09-22 00:27:47 +0200

    reverse finish.vue

--- resources/assets/js/views/wizard/Finish.vue
+++ resources/assets/js/views/wizard/Finish.vue
@@ -7,67 +7,48 @@
                 <span class="material-icons form-spin animate-spin text-9xl">data_usage</span>
             </div>
 
-            <div class="flex flex-col lg:flex-row mt-4">
+            <div class="flex flex-col lg:flex-row mt-6 mr-8">
                 <div class="w-full lg:w-1/2 ltr:pr-10 rtl:pl-10 mt-3">
-                    <div class="grid sm:grid-cols-6 overflow-y-scroll lg:overflow-hidden scroll">
-                        <h1 class="sm:col-span-6 text-black-300 mb-2 text-lg font-light">
-                            {{ translations.finish.recommended_apps }}
-                        </h1>
+                <div class="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
+                    <div class="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
+                    <h2 class="text-4xl font-bold sm:text-5xl">
+                        Ustvarite
+                        <span class="hidden sm:block text-4xl">
+                            svoj prvi račun
+                        </span>
+                    </h2>
 
-                        <div v-for="(item, index) in modules" :key="index" class="sm:col-span-6 mb-3 bg-default rounded-md">
-                            <a :href="route_url + '/apps/' + item.slug" class="flex items-center space-x-4 bg-purple-50 rounded-md p-2 sliding-app">
-                                <div class="w-1/4">
-                                <img v-for="(file, indis) in item.files" :key="indis" v-if="file.media_type == 'image' && file.pivot.zone == 'thumbnail'"
-                                    :src="file.path_string"
-                                    :alt="item.name"
-                                    class="rounded-lg object-cover ml-1 mt-0.5"
-                                    />
-                                </div>
-
-                                <div class="w-3/4">
-                                    <h4 class="font-medium text-sm pb-1">
-                                        {{ item.name }}
-                                    </h4>
-
-                                    <div class="text-black-300 text-sm mt-1 line-clamp-2 h-10 ltr:pr-0 rtl:pr-2" v-html="item.description"></div>
-                                </div>
-                            </a>
-                        </div>
+                    <base-button class="btn inline-flex items-center justify-center px-6 py-3 text-base disabled:opacity-50 relative mt-5 mx-auto bg-purple hover:bg-purple-700 disabled:bg-purple-700 text-white rounded-md py-3 px-5 font-semibold" @click="finish()">
+                    Naprej
+                    </base-button>
                     </div>
 
-                    <div class="lg:hidden">
-                        <base-button class="btn flex items-center justify-center text-base disabled:opacity-50 relative mt-5 mx-auto bg-green hover:bg-gray-100 text-white rounded-md py-3 px-5 font-medium" @click="finish()">
-                            {{ translations.finish.create_first_invoice }}
-                        </base-button>
-                    </div>
+
+                </div>
                 </div>
 
-                <div class="absolute w-1/2 ltr:right-0 rtl:left-0 ltr:pl-10 rtl:pr-10 mt-3 hidden lg:flex lg:flex-col">
-                    <div class="flex">
-                        <img :src="image_src" class="absolute top-12 ltr:right-2 rtl:left-2 w-10/12 p-3 -mt-12" alt="Akaunting" />
-                    </div>
+                <div class="absolute w-1/2 right-0 ltr:pl-10 rtl:pr-10 mt-3 hidden lg:flex lg:flex-col">
+                    <div class="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
+                    <img :src="image_src" class="absolute top-12 ltr:right-2 rtl:left-2 w-10/12 p-3 -mt-12" alt="Akaunting" />
+                </div>
 
-                    <div class="flex">
-                        <base-button
-                            class="relative rtl:right-20 flex items-center justify-center text-base rounded-lg m-auto mt-96 hover:underline py-1.5 -right-20 font-semibold disabled:bg-gray-100 "
-                            :disabled="anchor_loading"
-                            @click="finish()"
-                            style="top: 5.9rem"
-                        >
-                            <i v-if="anchor_loading" class="animate-submit delay-[0.28s] absolute w-2 h-2 rounded-full left-0 right-0 -top-2.5 m-auto before:absolute before:w-2 before:h-2 before:rounded-full before:animate-submit_second before:delay-[0.14s] after:absolute after:w-2 after:h-2 after:rounded-full after:animate-submit_second before:-left-3.5 after:-right-3.5 after:delay-[0.42s]"></i> 
-
-                            <span :class="[{'opacity-0': anchor_loading}]">
+                    <base-button
+                        class="relative flex items-center justify-center text-base rounded-lg m-auto bottom-48 bg-white hover:bg-gray-100 text-purple py-3 px-5 font-semibold disabled:bg-gray-100 "
+                        :disabled="anchor_loading"
+                        @click="finish()"
+                    >
+                        <i v-if="anchor_loading" class="animate-submit_second delay-[0.28s] absolute w-2 h-2 rounded-full left-0 right-0 -top-2.5 m-auto before:absolute before:w-2 before:h-2 before:rounded-full before:animate-submit_second before:delay-[0.14s] after:absolute after:w-2 after:h-2 after:rounded-full after:animate-submit_second before:-left-3.5 after:-right-3.5 after:delay-[0.42s]"></i>
+                        <span :class="[{'opacity-0': anchor_loading}]">
                                 {{ translations.company.skip }}
                             </span>
                         </base-button>
-
                         <base-button
-                            class="relative ltr:-right-2 rtl:-left-2 flex items-center justify-center text-base rounded-lg m-auto mt-96 bg-default hover:bg-default-hover text-white py-1.5 px-7 font-medium"
+                            class="relative ltr:-right-2 rtl:-left-2 flex items-center justify-center text-base rounded-lg m-auto mt-96 bg-purple hover:bg-purple-700 disabled:bg-purple-700 text-white py-1.5 px-7 font-medium"
                             :disabled="anchor_loading"
                             @click="finish()"
                             style="top: 5.9rem;"
                         >
-                            <i v-if="anchor_loading" class="animate-submit delay-[0.28s] absolute w-2 h-2 rounded-full left-0 right-0 -top-2.5 m-auto before:absolute before:w-2 before:h-2 before:rounded-full before:animate-submit_second before:delay-[0.14s] after:absolute after:w-2 after:h-2 after:rounded-full after:animate-submit_second before:-left-3.5 after:-right-3.5 after:delay-[0.42s]"></i> 
+                            <i v-if="anchor_loading" class="animate-submit_second delay-[0.28s] absolute w-2 h-2 rounded-full left-0 right-0 -top-2.5 m-auto before:absolute before:w-2 before:h-2 before:rounded-full before:animate-submit_second before:delay-[0.14s] after:absolute after:w-2 after:h-2 after:rounded-full after:animate-submit_second before:-left-3.5 after:-right-3.5 after:delay-[0.42s]"></i>
 
                             <span :class="[{'opacity-0': anchor_loading}]">
                                 {{ translations.finish.create_first_invoice }}
@@ -152,7 +133,7 @@ export default {
 <style scoped>
     .sliding-app:hover {
         animation: slidingAnimation 600ms ease-out forwards;
-    }   
+    }
 
     @keyframes slidingAnimation {
         0% { transform: translateX(0); }
